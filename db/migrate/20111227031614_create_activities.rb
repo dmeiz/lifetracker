06eec44e7697f85962df0c102b4349ab60f19035
Dan Hensgen <dan@methodhead.com>
2011-12-26 22:30:51 -0500

    Fix migration

--- db/migrate/20111227031614_create_activities.rb
+++ db/migrate/20111227031614_create_activities.rb
@@ -1,7 +1,7 @@
 class CreateActivities < ActiveRecord::Migration
   def change
     create_table :activities do |t|
-      t.int :category_id
+      t.integer :category_id
       t.datetime :start_at
       t.datetime :end_at
       t.string :memo
