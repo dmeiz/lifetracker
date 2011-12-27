b765d438e75bcff309707601a9833120db5cb48b
Dan Hensgen <dan@methodhead.com>
2011-12-27 00:51:52 -0500

    Good enough testing

--- test/tc_nothing.rb
+++ test/tc_nothing.rb
@@ -1,4 +1,6 @@
 require 'test/unit'
+ENV['RAILS_ENV'] = 'test'
+load File.expand_path('../../bin/lifetracker', __FILE__)
 
 class TC_testNothing < Test::Unit::TestCase
 
@@ -9,6 +11,7 @@ class TC_testNothing < Test::Unit::TestCase
   end
 
   def test_the_truth
-    assert true
+    GLI.run ['add', '12:00', '01:00', 'prj', 'Lifetracker - shoved off']
+    assert_equal 1, Activity.count
   end
 end
