646f590e35d2b6ed26fa77637e09cbeda99a8818
Dan Hensgen <dan@methodhead.com>
2011-12-30 00:10:08 -0500

    Assert stdout in a test
    Hoses debugger for some reason

--- test/unit/cli_test.rb
+++ test/unit/cli_test.rb
@@ -1,12 +1,20 @@
+@@orig_stdout = $stdout
 require 'test_helper'
 load File.expand_path('../../../bin/lifetracker', __FILE__)
 
 class CliTest < ActiveSupport::TestCase
   context 'add' do
-    should 'report errors' do
-    debugger
-      GLI.run ['add', '12:00', '01:00', 'prj', 'Lifetracker - shoved off']
-      assert_equal 0, Activity.count
+    setup do
+      @category = Category.create(:name => 'Cat1', :abbr => 'cat1')
+      $stdout = StringIO.new
+    end
+
+    should 'add an activity' do
+      GLI.run ['add', '12:00', '01:00', @category.abbr, 'Lifetracker - shoved off']
+      assert_equal 1, Activity.count
+      assert_equal "Added activity\n", $stdout.string
+      activity = Activity.last
+      assert_equal 'Lifetracker - shoved off', activity.memo
     end
   end
 end
