f372eafda4f797c9ef4078f1172535e2fdbe5d8a
Dan Hensgen <dan@methodhead.com>
2011-12-30 00:33:08 -0500

    Assert a whole activity

--- test/unit/cli_test.rb
+++ test/unit/cli_test.rb
@@ -7,13 +7,16 @@ class CliTest < ActiveSupport::TestCase
     setup do
       @category = Category.create(:name => 'Cat1', :abbr => 'cat1')
       $stdout = StringIO.new
+      @now = Time.now
     end
 
     should 'add an activity' do
-      GLI.run ['add', '12:00', '01:00', @category.abbr, 'Lifetracker - shoved off']
+      GLI.run ['add', @now.to_s('%h:%m'), (@now + 1.hour).to_s('%h:%m'), @category.abbr, 'Lifetracker - shoved off']
       assert_equal 1, Activity.count
       assert_equal "Added activity\n", $stdout.string
       activity = Activity.last
+      assert_equal @now.utc.to_i, activity.start_at.to_i
+      assert_equal (@now + 1.hour).to_i, activity.end_at.to_i
       assert_equal 'Lifetracker - shoved off', activity.memo
     end
   end
