fea0cdbfc2965cc956d5500e496de6a0c61b65c4
Dan Hensgen <dan@methodhead.com>
2011-12-30 14:20:40 -0500

    Use Chronic

--- test/unit/cli_test.rb
+++ test/unit/cli_test.rb
@@ -6,18 +6,28 @@ class CliTest < ActiveSupport::TestCase
   context 'add' do
     setup do
       @category = Category.create(:name => 'Cat1', :abbr => 'cat1')
-      $stdout = StringIO.new
+      #$stdout = StringIO.new
       @now = Time.now
+      @command = ['add', @now.to_s('%h:%m'), (@now + 1.hour).to_s('%h:%m'), @category.abbr, 'Lifetracker - shoved off']
     end
 
     should 'add an activity' do
-      GLI.run ['add', @now.to_s('%h:%m'), (@now + 1.hour).to_s('%h:%m'), @category.abbr, 'Lifetracker - shoved off']
+      GLI.run @command
       assert_equal 1, Activity.count
       assert_equal "Added activity\n", $stdout.string
       activity = Activity.last
-      assert_equal @now.utc.to_i, activity.start_at.to_i
+      assert_equal @now.to_i, activity.start_at.to_i
       assert_equal (@now + 1.hour).to_i, activity.end_at.to_i
       assert_equal 'Lifetracker - shoved off', activity.memo
+    end
+
+    should 'use chronic' do
+      @command[1] = 'now'
+      @command[2] = 1.hour.from_now.to_s('%I%p') # "1pm"
+      GLI.run @command
+      activity = Activity.last
+      assert_equal @now.to_i, activity.start_at.to_i
+      assert_equal 1.hour.from_now.to_i, activity.end_at.to_i
     end
   end
 end
