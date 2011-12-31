611b030986629e056cdf882cdbf70d136134442e
Dan Hensgen <dan@methodhead.com>
2011-12-31 18:11:57 -0500

    Day#parse

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,2 +1,29 @@
 class Day < ActiveRecord::Base
+  has_many :activities
+
+  def parse(text)
+    activity_lines(text).map {|line| line_to_hash(line)}
+  end
+
+  private
+
+  # Strip text of anything above and below the separator lines.
+  # 
+  def activity_lines(text)
+    lines = text.split("\n")
+    lines = lines.drop_while {|line| line !~ /^[- ]+$/ }
+    lines = lines.drop(1)
+    lines = lines.take_while {|line| line !~ /^[- ]+$/ }
+    lines
+  end
+
+  def line_to_hash(line)
+    tokens = line.split(/\s+/)
+    {
+      :start_at => Chronic.parse(tokens[0]),
+      :end_at => Chronic.parse(tokens[1]),
+      :category => Category.find_by_abbr(tokens[3].downcase),
+      :memo => tokens[4..-1].join(' ')
+    }
+  end
 end
--- test/unit/day_test.rb
+++ test/unit/day_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class DayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context 'parse' do
+    setup do
+      @category = Category.create(:abbr => 'per')
+      @day = Day.new
+      @text = <<END
+some arbitrary header text
+
+Start End   Dur   Cat    Memo
+------ ----- ----- ---    ------------------
+08:15a 08:45a 0.5hr PER    Morning routine
+08:45a 9     0.5hr PER    Morning routine
+------ ----- ----- ---    ------------------
+          1.0hr
+
+arbitrary footer text
+END
+    end
+
+    should 'parse a day' do
+      assert data = @day.parse(@text)
+      assert_equal 2, data.length
+      assert_equal({:start_at => Chronic.parse('8:15am'), :end_at => Chronic.parse('8:45am'), :category => @category, :memo => 'Morning routine'}, data.first)
+      assert_equal({:start_at => Chronic.parse('8:45am'), :end_at => Chronic.parse('9:00am'), :category => @category, :memo => 'Morning routine'}, data.last)
+    end
+
+    # TODO: replace existing entries
+    # TODO: report parse error
+  end
 end
