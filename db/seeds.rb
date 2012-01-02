3f1bec49bf8c242a35cc1052642391f1b520f553
Dan Hensgen <dan@methodhead.com>
2012-01-02 11:29:46 -0500

    Move populate to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+{
+  # work tasks
+  :per => 'Personal',
+  :sto => 'Story',
+  :un => 'Uncategorized',
+  :oh => 'Overhead',
+  :bug => 'Bugfix',
+
+  # personal tasks
+  :sle => 'Sleep',
+  :eat => 'Eating',
+  :par => 'Parenting',
+  :prj => 'Personal project',
+  :rel => 'Relaxing',
+  :soc => 'Socializing',
+  :cho => 'Chore'
+}.each do |abbr, name|
+  Category.create(:abbr => abbr, :name => name)
+end
+
+# a little development data in here too :(
+unless Rails.env.production?
+  day = Day.create(:dt => Date.today)
+
+  day.activities.create!(
+    :start_at => Time.now,
+    :end_at => Time.now + 1.hour,
+    :category => Category.find_by_abbr('per'),
+    :memo => 'Sleeping'
+  )
+end
--- lib/tasks/lifetracker.rake
+++ lib/tasks/lifetracker.rake
@@ -1,25 +1,8 @@
 namespace :lifetracker do
+=begin for example...
   desc 'Populate database with categories'
   task :populate => :environment do
-    Category.delete_all
-    {
-      # work tasks
-      :per => 'Personal',
-      :sto => 'Story',
-      :un => 'Uncategorized',
-      :oh => 'Overhead',
-      :bug => 'Bugfix',
-
-      # personal tasks
-      :sle => 'Sleep',
-      :eat => 'Eating',
-      :par => 'Parenting',
-      :prj => 'Personal project',
-      :rel => 'Relaxing',
-      :soc => 'Socializing',
-      :cho => 'Chore'
-    }.each do |abbr, name|
-      Category.create(:abbr => abbr, :name => name)
-    end
+    ...
   end
+=end
 end
