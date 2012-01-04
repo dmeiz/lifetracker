06593a11ef2f2e6650d08581e356df49b2374cef
Dan Hensgen <dan@methodhead.com>
2012-01-04 13:07:54 -0500

    Fix 12am end_at problem

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,10 +2,23 @@ class Activity < ActiveRecord::Base
   belongs_to :category
   belongs_to :day
 
-  validates_presence_of :memo, :category
+  validates_presence_of :start_at, :end_at, :category
+
+  before_save :fix_midnight_end_at
 
   def duration_in_hours
     return nil unless self.start_at && self.end_at
     (self.end_at - self.start_at).to_f/1.hour
+  end
+
+private
+
+  # A 12AM end_at at the end of the day often get parsed as the beginning of
+  # the day. Force it to the end of the day.
+  #
+  def fix_midnight_end_at
+    if self.end_at == self.start_at.beginning_of_day
+      self.end_at += 24.hours
+    end
   end
 end
--- test/unit/activity_test.rb
+++ test/unit/activity_test.rb
@@ -1,12 +1,24 @@
 require 'test_helper'
 
 class ActivityTest < ActiveSupport::TestCase
-  should validate_presence_of :memo
   should validate_presence_of :category
+  should validate_presence_of :start_at
+  should validate_presence_of :end_at
+
 
   context 'validations' do
     should 'assert truth' do
       assert true
+    end
+  end
+
+  context 'callbacks' do
+    should 'adjust end_at to correctly handle 12am' do
+      @category = Category.create!(:name => 'Foo', :abbr => 'foo')
+      @start_at = Time.local(2012, 1, 4, 11, 0, 0)
+      @end_at = Time.local(2012, 1, 4, 0, 0, 0)
+      @activity = Activity.create!(:start_at => @start_at, :end_at => @end_at, :category => @category, :memo => 'Memo')
+      assert_equal (@end_at + 24.hours).to_i, @activity.end_at.to_i
     end
   end
 
